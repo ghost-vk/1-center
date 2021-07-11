b6b454e92842577ba032d0b24207d4794ffc09e6
ghost <ghost.vkv@gmail.com>
2021-07-11 12:06:24 +0300

    Minified JS

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,50 @@ const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 
+const isDev = process.env.NODE_ENV === 'development';
+const isProduction = !isDev;
+
+const devOptimization = {
+    splitChunks: {
+        chunks: 'all'
+    }
+}
+
+const prodOptimization = {
+    minimize: isProduction,
+    minimizer: [
+        new TerserPlugin()
+    ],
+}
+
+const optimization = isProduction ? prodOptimization : devOptimization
+
+const devRules = [
+    {
+        test: /\.css$/,
+        use: [
+            MiniCssExtractPlugin.loader,
+            "css-loader", "postcss-loader",
+        ],
+    },
+]
+
+const prodRules = [
+    ...devRules,
+    {
+        test: [/\.js?$/, /\.ts?$/, /\.jsx?$/, /\.tsx?$/],
+        exclude: /(node_modules|bower_components)/,
+        use: {
+            loader: 'babel-loader',
+            options: {
+                presets: ["@babel/preset-env"]
+            }
+        }
+    },
+]
+
+const rules = isProduction ? prodRules : devRules
+
 module.exports = {
     entry: {
         app: path.resolve(__dirname, './src/js/app.js')
@@ -11,26 +55,14 @@ module.exports = {
         filename: '[name].bundle.js',
         chunkFilename: '[id].js',
     },
-    optimization: {
-        splitChunks: {
-            chunks: 'all'
-        }
-    },
+    optimization: optimization,
     module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    MiniCssExtractPlugin.loader,
-                    "css-loader", "postcss-loader",
-                ],
-            },
-        ]
+        rules: rules
     },
     plugins: [
         new MiniCssExtractPlugin({
             filename: "styles.css",
             chunkFilename: "styles.css"
         }),
-    ]
+    ],
 };
